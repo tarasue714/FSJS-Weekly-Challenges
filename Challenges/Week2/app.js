7762d4431827f661a73afcc86ff9525cb8365b25
Tara Chenault <tara.chenaut714@live.com>
2016-09-07 19:52:40 -0400

    Complete Challenege for Week 2

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,4 +1,5 @@
 //create function
+
   //control and return statements
 
 //Prompt user for input and store variable
@@ -6,3 +7,25 @@
 //Call function
 
 //Alert user results
+
+
+message = prompt();
+
+processMessage(message);
+
+function processMessage(msg) 
+{
+	if (message === "Hello") {
+		console.log("Correct");
+
+	} else {
+
+	console.log("Rude");
+	}
+}
+
+
+
+var message;
+
+message = prompt();
